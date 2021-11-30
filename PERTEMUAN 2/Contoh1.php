d36759f9118b14f66cd01d50ac32474f37416e27
dtnasuha <90677225+dtnasuha@users.noreply.github.com>
2021-11-30 15:19:52 +0700

    Add files via upload
    pertemuan ke 2

---
+++ PERTEMUAN 2/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+
+ 	public function index()
+ 	{
+ 		echo "<h1>Perkenalkan</h1>";
+ 		echo"Nama saya andika dwi
+			 Saya tingga di daerah bintaro
+			 olah raga yang saya sukai adalah
+ 			 Bulutangkis";
+ 	}
+}
