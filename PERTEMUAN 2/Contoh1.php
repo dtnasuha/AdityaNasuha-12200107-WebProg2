bd20d6aba7fa2c971eb309e9d0f0b374a06c5843
dtnasuha <90677225+dtnasuha@users.noreply.github.com>
2021-11-30 15:23:49 +0700

    Update Contoh1.php

--- PERTEMUAN 2/Contoh1.php
+++ PERTEMUAN 2/Contoh1.php
@@ -5,9 +5,9 @@ class Contoh1 extends CI_Controller
  	public function index()
  	{
  		echo "<h1>Perkenalkan</h1>";
- 		echo"Nama saya andika dwi
-			 Saya tingga di daerah bintaro
+ 		echo"aditya
+			 Saya tingga di daerah depok
 			 olah raga yang saya sukai adalah
- 			 Bulutangkis";
+ 			 basket";
  	}
 }
